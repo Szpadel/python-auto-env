3e9644d51ec012547cab7c31a49a1e6bd16d1da4
Piotr Rogowski <piotrekrogowski@gmail.com>
2020-09-24 17:35:15 +0200

    Use os.path.join in example for windows compatibility

--- example.py
+++ example.py
@@ -4,14 +4,14 @@ import os
 current_file = os.path.abspath(__file__)
 current_dir = os.path.dirname(current_file)
 auto_env.run_in_venv(
-    "%s/.venv" % current_dir, # Path to virtual env
-    "%s/requirements.txt" % current_dir, # Path to requirements.txt
+    os.path.join(current_dir, '.venv'), # Path to virtual env
+    os.path.join(current_dir, 'requirements.txt'), # Path to requirements.txt
     current_file # Path to file to execute
 )
 
 
 
-import numpy as np;
+import numpy as np
 
 print("Hello world")
 print(np.array([1,2,3]))
